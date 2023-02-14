435b7d24a3f61c76c445749e3c5509b6256ce5d3
Samuel Rojas <frojas@occ.com.mx>
2023-02-13 22:44:53 -0600

    Create Pipeline with AWS CDK

---
+++ pipeline_stack.py
@@ -0,0 +1,25 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    pipelines as pipelines,
+)
+
+class WorkshopPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Pipeline code will go here
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.code_commit(repo, "main"),
+                commands=[
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "cdk synth",
+                ]
+            ),
+        )
